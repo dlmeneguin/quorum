f922f6d4918b76e7c3438c116e084db1fea24d8c
dlmeneguin <dl.meneguin@gmail.com>
2026-03-30 18:54:42 -0300

    import fix

--- lib/features/budget/screens/budget_screen.dart
+++ lib/features/budget/screens/budget_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../../core/database/app_database.dart';
 import '../../../core/database/database_provider.dart';
 import '../../../core/utils/currency.dart';
 import '../../../shared/theme/app_colors.dart';
