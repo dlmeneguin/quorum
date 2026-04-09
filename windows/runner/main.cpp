caef67785700fab6641d11b60b5598871fdcca61
dlmeneguin <dl.meneguin@gmail.com>
2026-04-09 13:20:57 -0300

    Fixes for the release versions

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"Quórum", origin, size)) {
+  if (!window.Create(L"Qu\u00f3rum", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
